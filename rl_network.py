80ed0038276f78bd050a2a89d793311acc0d4da6
yl408 <yl408@duke.edu>
2018-11-14 10:19:09 -0500

    update: converge

--- rl_network.py
+++ rl_network.py
@@ -1,138 +1,203 @@
+"""
+This part of code is the DQN brain, which is a brain of the agent.
+All decisions are made in here.
+Using Tensorflow to build the neural network.
+View more on my tutorial page: https://morvanzhou.github.io/tutorials/
+Using:
+Tensorflow: 1.0
+gym: 0.7.3
+"""
+
 import numpy as np
-import tensorflow as tf
-from collections import OrderedDict
 import pandas as pd
-import random
-import matplotlib.pyplot as plt
+import tensorflow as tf
 
+np.random.seed(1)
+tf.set_random_seed(1)
+
+
+# Deep Q Network off-policy
 class DeepQNetwork:
-	def __init__(
-		self,
-		n_actions,
-		n_features,
-		learnig_rate = 0.01,
-		reward_decay = 0.9,
-		e_greedy = 0.9,
-		memory_size = 500,
-		replace_target_iter = 300,
-		epsilon_increment = None,
-		batch_size = 32,
-		):
-		self.n_actions = n_actions;
-		self.n_features = n_features;
-		self.learnig_rate = learnig_rate;
-		self.gamma = reward_decay;
-		self.epsilon_max = e_greedy;
-		self.memory_size = memory_size
-		self.batch_size = batch_size
-		self.memory = np.array((self.memory_size,n_features*2+2))
-		self.replace_target_iter = replace_target_iter
+    def __init__(
+            self,
+            n_actions,
+            n_features,
+            learning_rate=0.01,
+            reward_decay=0.9,
+            e_greedy=0.9,
+            replace_target_iter=300,
+            memory_size=500,
+            batch_size=32,
+            e_greedy_increment=None,
+            output_graph=False,
+    ):
+        self.n_actions = n_actions
+        self.n_features = n_features
+        self.lr = learning_rate
+        self.gamma = reward_decay
+        self.epsilon_max = e_greedy
+        self.replace_target_iter = replace_target_iter
+        self.memory_size = memory_size
+        self.batch_size = batch_size
+        self.epsilon_increment = e_greedy_increment
+        self.epsilon = 0 if e_greedy_increment is not None else self.epsilon_max
 
-		self.epsilon_increment = epsilon_increment
-		self.epsilon = 0 if self.epsilon_increment is not None else self.epsilon_max
+        # total learning step
+        self.learn_step_counter = 0
 
+        # initialize zero memory [s, a, r, s_]
+        self.memory = np.zeros((self.memory_size, n_features * 2 + 2))
 
-	###create a session
+        # consist of [target_net, evaluate_net]
+        self._build_net()
+        t_params = tf.get_collection('target_net_params')
+        e_params = tf.get_collection('eval_net_params')
+        self.replace_target_op = [tf.assign(t, e) for t, e in zip(t_params, e_params)]
 
-		self.learn_step_counter = 0
-		self.cost_hist = list()
-		self.sess = tf.Session()
+        self.sess = tf.Session()
 
-		self._build_net()
-		e_params = tf.get_collection('eval_net_params')
-		t_params = tf.get_collection('target_net_params')
-		self.replace_target_op = [tf.assign(t,e) for t,e in zip(t_params,e_params)]
-	
-		self.sess.run(tf.global_variables_initializer())
-	def _build_net(self):
-		###eval
-		self.s = tf.placeholder(tf.float32,[None,self.n_features],name='s')
-		self.q_target = tf.placeholder(tf.float32,[None,self.n_actions],name='Q_target')
-		with tf.variable_scope('eval_net'):
-			c_names = ['eval_net_params',tf.GraphKeys.GLOBAL_VARIABLES]
-			n_l1 = 10
-			w_initializer = tf.random_normal_initializer(0.0,0.3)
-			b_initializer = tf.constant_initialzier(0.1)
+        if output_graph:
+            # $ tensorboard --logdir=logs
+            # tf.train.SummaryWriter soon be deprecated, use following
+            tf.summary.FileWriter("logs/", self.sess.graph)
 
-			with tf.variable_scope('l1'):
-				w1 = tf.get_variable('w1',[self.n_features,n_l1],initializer=w_initializer,collections=c_names)
-				b1 = tf.get_variable('b1',[1,n_l1],initializer=b_initializer,collections=c_names)
-				l1 = tf.nn.relu(tf.matmul(self.s,w1)+b1)
+        self.sess.run(tf.global_variables_initializer())
+        self.cost_his = []
 
-			with tf.variable_scope('l2'):
-				w2 = tf.get_variable('w2',[self.n_l1,self.n_actions],initializer=w_initializer,collections=c_names)
-				b2 = tf.get_variable('b1',[1,self.n_actions],initializer=b_initializer,collections=c_names)
-				self.q_eval = tf.matmul(l1,w2) + b2
+    def _build_net(self):
+        # ------------------ build evaluate_net ------------------
+        self.s = tf.placeholder(tf.float32, [None, self.n_features], name='s')  # input
+        self.q_target = tf.placeholder(tf.float32, [None, self.n_actions], name='Q_target')  # for calculating loss
+        with tf.variable_scope('eval_net'):
+            # c_names(collections_names) are the collections to store variables
+            c_names, n_l1, w_initializer, b_initializer = \
+                ['eval_net_params', tf.GraphKeys.GLOBAL_VARIABLES], 10, \
+                tf.random_normal_initializer(0., 0.3), tf.constant_initializer(0.1)  # config of layers
 
-			with tf.variable_scope('loss'):
-				self.loss = tf.reduce_mean(tf.squared_difference(self.q_eval,self.q_target))
-			with tf.variable_scope('train'):
-				self._train_op = tf.train.RMSPropOptimizer(self.lr).minimize(self.loss)
+            # first layer. collections is used later when assign to target net
+            with tf.variable_scope('l1'):
+                w1 = tf.get_variable('w1', [self.n_features, n_l1], initializer=w_initializer, collections=c_names)
+                b1 = tf.get_variable('b1', [1, n_l1], initializer=b_initializer, collections=c_names)
+                l1 = tf.nn.relu(tf.matmul(self.s, w1) + b1)
 
-		self.s_ = tf.placeholder(tf.float32,[None,self.n_features],name='s')
+            # second layer. collections is used later when assign to target net
+            with tf.variable_scope('l2'):
+                w2 = tf.get_variable('w2', [n_l1, self.n_actions], initializer=w_initializer, collections=c_names)
+                b2 = tf.get_variable('b2', [1, self.n_actions], initializer=b_initializer, collections=c_names)
+                self.q_eval = tf.matmul(l1, w2) + b2
 
-		with tf.variable_scope('target_net'):
-			c_naemes = ['target_net_params',tf.GraphKeys.GLOBAL_VARIABLES]
+        with tf.variable_scope('loss'):
+            self.loss = tf.reduce_mean(tf.squared_difference(self.q_target, self.q_eval))
+        with tf.variable_scope('train'):
+            self._train_op = tf.train.RMSPropOptimizer(self.lr).minimize(self.loss)
 
+        # ------------------ build target_net ------------------
+        self.s_ = tf.placeholder(tf.float32, [None, self.n_features], name='s_')    # input
+        with tf.variable_scope('target_net'):
+            # c_names(collections_names) are the collections to store variables
+            c_names = ['target_net_params', tf.GraphKeys.GLOBAL_VARIABLES]
 
-			with tf.variable_scope('l1'):
-				w1 = tf.get_variable('w1',[self.n_features,n_l1],initializer=w_initializer,collections=c_names)
-				b1 = tf.get_variable('b1',[1,n_l1],initializer=b_initializer,collections=c_names)
-				l1 = tf.nn.relu(tf.matmul(self.s_,w1)+b1)
+            # first layer. collections is used later when assign to target net
+            with tf.variable_scope('l1'):
+                w1 = tf.get_variable('w1', [self.n_features, n_l1], initializer=w_initializer, collections=c_names)
+                b1 = tf.get_variable('b1', [1, n_l1], initializer=b_initializer, collections=c_names)
+                l1 = tf.nn.relu(tf.matmul(self.s_, w1) + b1)
 
-			with tf.variable_scope('l2'):
-				w2 = tf.get_variable('w2',[self.n_l1,self.n_actions],initializer=w_initializer,collections=c_names)
-				b2 = tf.get_variable('b1',[1,self.n_actions],initializer=b_initializer,collections=c_names)
-				self.q_next = tf.matmul(l1,w2) + b2
+            # second layer. collections is used later when assign to target net
+            with tf.variable_scope('l2'):
+                w2 = tf.get_variable('w2', [n_l1, self.n_actions], initializer=w_initializer, collections=c_names)
+                b2 = tf.get_variable('b2', [1, self.n_actions], initializer=b_initializer, collections=c_names)
+                self.q_next = tf.matmul(l1, w2) + b2
 
-	def store_transition(self,s,a,s_,r):
-		if not hasattr(self,'memory_counter'):
-			self.memory_counter = 0
+    def store_transition(self, s, a, r, s_):
+        if not hasattr(self, 'memory_counter'):
+            self.memory_counter = 0
 
-		index = self.memory_counter % self.memory.memory_size
-		self.memory[index,:] = np.array(s,a,r,s_)
-		self.memory_counter += 1
+        transition = np.hstack((s, [a, r], s_))
 
-	def choose_action(self,state):
-		q_val = self.sess.run(self.q_eval,feed_dict={self.s:state})
-		a_max = np.argmax(q_val)
-		if np.random.uniform() > e_greedy:
-			a_max = np.random.randint(0,self.n_actions)
-		return a_max
+        # replace the old memory with new memory
+        index = self.memory_counter % self.memory_size
+        self.memory[index, :] = transition
 
-	def learn(self):
-		if (self.learn_step_counter % self.replace_target_iter == 0):
-			self.sess.run(self.replace_target_op)
-			print("\ntaget params updated \n")
-		if (self.memory_counter > self.memory_size):
-			batch_index = np.random.choice(self.memory_counter,self.batch_size)
-		else:
-			batch_index = np.random.choice(self.memory_size,self.batch_size)
+        self.memory_counter += 1
 
-		batch_memory = self.memory[batch_index,:]
-		q_next = self.sess.run(self.q_next,feed_dict={self.s_:batch_memory[:,-n_features:]})
-		q_eval = self.sess_run(self.q_eval,fedd_dict={self.s:batch_memory[:,:n_features]})
+    def choose_action(self, observation):
+        # to have batch dimension when feed into tf placeholder
+        observation = observation[np.newaxis, :]
 
+        if np.random.uniform() < self.epsilon:
+            # forward feed the observation and get q value for every actions
+            actions_value = self.sess.run(self.q_eval, feed_dict={self.s: observation})
+            action = np.argmax(actions_value)
+        else:
+            action = np.random.randint(0, self.n_actions)
+        return action
 
-		q_target = q_eval.copy()
-		eval_act_index = batch_memory[:,self.n_features].astype(int)
-		reward = batch_memory[:,self.n_features+1]
-		indexs = np.arange(batch_size,dtype=np.int32)
-		q_target[indexs,eval_act_index] = reward + self.gamma*np.max(q_next,axis=1)
+    def learn(self):
+        # check to replace target parameters
+        if self.learn_step_counter % self.replace_target_iter == 0:
+            self.sess.run(self.replace_target_op)
+            #print('\ntarget_params_replaced\n')
 
-		###
-		_,self.cost = self.sess.run(self._train_op,self.loss,feed_dict={self.s:batch_memory[:,n_features:],self.q_target:q_target})
+        # sample batch memory from all memory
+        if self.memory_counter > self.memory_size:
+            sample_index = np.random.choice(self.memory_size, size=self.batch_size)
+        else:
+            sample_index = np.random.choice(self.memory_counter, size=self.batch_size)
+        batch_memory = self.memory[sample_index, :]
 
-		self.cost_hist.append(his)
-		self.epsilon = self.epsilon + self.epsilon_increment if self.epsilon < self.epsilon_max else self.epsilon_max
-		self.learn_step_counter += 1
+        q_next, q_eval = self.sess.run(
+            [self.q_next, self.q_eval],
+            feed_dict={
+                self.s_: batch_memory[:, -self.n_features:],  # fixed params
+                self.s: batch_memory[:, :self.n_features],  # newest params
+            })
 
-	def plot_lost(self):
-		plt.plot(self.cost_hist)
-		plt.show()
+        # change q_target w.r.t q_eval's action
+        q_target = q_eval.copy()
 
+        batch_index = np.arange(self.batch_size, dtype=np.int32)
+        eval_act_index = batch_memory[:, self.n_features].astype(int)
+        reward = batch_memory[:, self.n_features + 1]
 
+        q_target[batch_index, eval_act_index] = reward + self.gamma * np.max(q_next, axis=1)
 
+        """
+        For example in this batch I have 2 samples and 3 actions:
+        q_eval =
+        [[1, 2, 3],
+         [4, 5, 6]]
+        q_target = q_eval =
+        [[1, 2, 3],
+         [4, 5, 6]]
+        Then change q_target with the real q_target value w.r.t the q_eval's action.
+        For example in:
+            sample 0, I took action 0, and the max q_target value is -1;
+            sample 1, I took action 2, and the max q_target value is -2:
+        q_target =
+        [[-1, 2, 3],
+         [4, 5, -2]]
+        So the (q_target - q_eval) becomes:
+        [[(-1)-(1), 0, 0],
+         [0, 0, (-2)-(6)]]
+        We then backpropagate this error w.r.t the corresponding action to network,
+        leave other action as error=0 cause we didn't choose it.
+        """
 
+        # train eval network
+        _, self.cost = self.sess.run([self._train_op, self.loss],
+                                     feed_dict={self.s: batch_memory[:, :self.n_features],
+                                                self.q_target: q_target})
+        self.cost_his.append(self.cost)
 
+        # increasing epsilon
+        self.epsilon = self.epsilon + self.epsilon_increment if self.epsilon < self.epsilon_max else self.epsilon_max
+        self.learn_step_counter += 1
+
+    def plot_cost(self):
+        import matplotlib.pyplot as plt
+        plt.plot(np.arange(len(self.cost_his)), self.cost_his)
+        plt.ylabel('Cost')
+        plt.xlabel('training steps')
+        plt.show()
 
--- simulate.py
+++ simulate.py
@@ -5,21 +5,29 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 import time
+import tensorflow as tf
 
-TaskQueue_Size = 10
+
+TaskQueue_Size = 5
 TaskQueue = list()
 ExecutionList = list()
 
 ## re[0] cpu re[1] mem
 resource_pool = np.zeros(2)
-resource_pool[0] = 10
-resource_pool[1] = 30
+resource_pool[0] = 1.0
+resource_pool[1] = 1.0
 
 
+short_task = 0
+long_task = 0
+
+num_of_traj = 10
 global_id = 0
 Pgt = PolicyGradient(n_actions = TaskQueue_Size+1,
-					 n_features = TaskQueue_Size*4+2,
-					 learning_rate = 0.01)
+					 n_features = TaskQueue_Size*2+2,
+					 learning_rate = 0.001,num_traj = num_of_traj,name="cloudlet")
+
+DQN = DeepQNetwork(n_actions = TaskQueue_Size+1, n_features = TaskQueue_Size*2+2,learning_rate = 1e-3)
 
 class Task:
 	def __init__(self,
@@ -37,30 +45,40 @@ class Task:
 		self.id = id
 
 def genshortTask(id):
-	task = Task(execution_Time = 1, cpu_demand=1,memory_demand=2,response_time = 1, deadline=100,id=id)
+	et = np.random.choice([1,2,3])
+	cpu_demand = np.random.uniform(0.01,0.1)
+	task = Task(execution_Time = et, cpu_demand=cpu_demand,memory_demand=cpu_demand,response_time = 1, deadline=100,id=id)
 	return task
 
 def genlongTask(id):
-	task = Task(execution_Time = 3, cpu_demand=2,memory_demand=4,response_time = 1, deadline=150,id=id)
+	et = np.random.choice([5,6,7,8])
+	cpu_demand = np.random.uniform(0.15,0.2)
+	task = Task(execution_Time = et, cpu_demand=cpu_demand,memory_demand=cpu_demand,response_time = 1, deadline=150,id=id)
 	return task
 
 def genTask(l):
 	global global_id
-	s = int(np.random.poisson(l,1))
+	#s = int(np.random.poisson(l,1))
+	s = 0
+	for i in range(l):
+		s += np.random.choice([0,1])
+	#print(s)
 	for i in range(s):
-		if(np.random.uniform() < 0.5):
+		if(np.random.uniform() < 0.8):
 			task = genshortTask(global_id)
 		else:
 			task = genlongTask(global_id)
-		if(len(TaskQueue) < TaskQueue_Size):
-			TaskQueue.append(task)
-			global_id +=1
+		#if(len(TaskQueue) < TaskQueue_Size):
+		TaskQueue.append(task)
+		global_id +=1
 
 def rewardsignal(EQ):
-	count = len(TaskQueue)
+	count = len(TaskQueue)#+len(ExecutionList)
 	Sum = 0.0
 	for T in TaskQueue:
 		Sum +=  T.response_time
+	#for task in ExecutionList:
+	#	Sum += task.response_time
 	#for et in EQ:
 	#	if et.execution_Time == 0:
 	#		print("removing!!!")
@@ -68,9 +86,13 @@ def rewardsignal(EQ):
 	#		resource_pool[0] += et.cpu_demand
 	#		resource_pool[1] += et.memory_demand
 	#		count += 1
-	return -Sum, Sum/count
+	if(count == 0.0):
+		count = 0.5
+	return -Sum, 1.0/count
 
 def progress(TQ,EQ):
+	total_response = 0.0
+	count = 0.0
 	for pd in TQ:
 		pd.response_time += 1
 	for et in EQ:
@@ -82,68 +104,294 @@ def progress(TQ,EQ):
 			EQ.remove(et)
 			resource_pool[0] += et.cpu_demand
 			resource_pool[1] += et.memory_demand
+			total_response+=et.response_time
+			count+=1
+	return total_response,count
 
 def execution(action):
-	if(action > len(TaskQueue) or action == 0):
-		return False
+	if(action >= min(len(TaskQueue),TaskQueue_Size)):
+		return 0
 
-	task = TaskQueue[action-1]
-	if task.cpu_demand < resource_pool[0] and task.memory_demand < resource_pool[1]:
-		TaskQueue.remove(task)
-		resource_pool[0] -= task.cpu_demand
-		resource_pool[1] -= task.memory_demand
-		ExecutionList.append(task)
-		return True
-	return False
+
+	task = TaskQueue[action]
+	if task.memory_demand > resource_pool[1]:
+		return 0
+	#if task.cpu_demand <= resource_pool[0] and task.memory_demand <= resource_pool[1]:
+		
+	TaskQueue.remove(task)
+	resource_pool[0] -= task.cpu_demand
+	resource_pool[1] -= task.memory_demand
+	ExecutionList.append(task)
+	global short_task
+	global long_task
+	if(task.execution_Time == 1):
+		short_task += 1
+	else:
+		long_task += 1
+	return 1
+	#return False
 def getObservation():
 	observation = list()
+	executable_task = list()
+	#for task in TaskQueue:
+		#if task.cpu_demand <= resource_pool[0] and task.memory_demand <= resource_pool[1]:
+	#	if task.memory_demand <= resource_pool[1]:
+	#		executable_task.append(task)
 
-	for task in TaskQueue:
-		observation.append(task.execution_Time)
-		observation.append(task.cpu_demand)
-		observation.append(task.memory_demand)
-		observation.append(task.response_time)
+	for i in range(min(len(TaskQueue),TaskQueue_Size)):
+		observation.append(TaskQueue[i].memory_demand)
+		observation.append(TaskQueue[i].execution_Time/10.0)
+
 	size = len(TaskQueue)
-	for i in range(TaskQueue_Size-size):
-		for j in range(4):
-			observation.append(0)
-	observation.append(resource_pool[0])
+	for i in range(max(TaskQueue_Size - size,0)):
+		for j in range(2):
+			observation.append(0.0)
+	left_num = max(len(TaskQueue)-TaskQueue_Size,0)
+	#print(left_num)
+	##observation.append(left_num)
+	#observation.append(resource_pool[0])
 	observation.append(resource_pool[1])
+	observation.append(min(left_num,15)/15.0)
 	return np.array(observation)
+
+#rw = []
+#ct = []
+def train():
+
+	global num_of_traj
+	obs = [list()]*num_of_traj
+	acts = [list()]*num_of_traj
+	rws = [list()]*num_of_traj
+	
+	np.random.seed(1)
+	for num in range(num_of_traj):
+
+		iteration = 50
+		avgs = []
+		counts = []
+		actions = [0]*TaskQueue_Size
+		rewards = [0]*TaskQueue_Size
+		global resource_pool
+		global TaskQueue
+		global ExecutionList
+		resource_pool[0] = 1.0
+		resource_pool[1] = 1.0
+		TaskQueue = list()
+		ExecutionList = list()
+			#np.random.seed(ID)
+		resp = 0.0
+		count = 0.0
+		for times in range(iteration):
+			resp_new,count_new = progress(TaskQueue,ExecutionList)
+			resp += resp_new
+			count += count_new
+			genTask(2)
+			#print("here!!!!")
+			#if(len(TaskQueue) > 15):
+				#print(times,"break")
+			#	break
+			while(True):
+				observation = getObservation()
+
+				action = Pgt.choose_action(observation)
+				#for i in range(min(len(exec_list),10)):
+				#	print(exec_list[i].execution_Time)
+				#print(len(exec_list),action,resource_pool[0],resource_pool[1])
+				#print(action,len(TaskQueue),resource_pool[0],resource_pool[1])
+			#	action = 1
+				#print(action)
+				actions[min(action,TaskQueue_Size-1)] += 1
+				result = execution(action) 
+
+
+				reward,average = rewardsignal(ExecutionList)
+				avgs.append(average)
+
+				counts.append(average)
+
+				reward = average - 0.1
+				if(reward < 0):
+					reward = reward*10
+				rewards[min(action,TaskQueue_Size-1)] += reward
+
+
+
+				obs[num].append(observation)
+				acts[num].append(action)
+				rws[num].append(reward)
+				if(result == 0):
+					break
+
+		#print(resp,count)
+	rewards = []
+	for num in range(num_of_traj):
+		for i in range(len(obs[num])):
+			Pgt.store_transition(obs[num][i],acts[num][i],rws[num][i])
+		#print("begin learn")
+		#l1,l2 = Pgt.get_weights()
+		#print(l2)
+		Vt = Pgt.learn()
+		#print("after learn")
+		#l1,l2 = Pgt.get_weights()
+		#print(l2)
+	rewards.append(Vt[0])
+	#rw.append(sum(avgs)/len(avgs))
+
+	return actions,rewards
+
+
+
+def eval_policy():
+	iteration = 2
+	counts = []
+	actions = [0]*(TaskQueue_Size+1)
+	for iter in range(iteration):
+		iteration = 300
+		np.random.seed(10)
+		global resource_pool
+		global TaskQueue
+		global ExecutionList
+		resource_pool[0] = 1.0
+		resource_pool[1] = 1.0
+		TaskQueue = list()
+		ExecutionList = list()
+
+		#cts = []
+		resp = 0.0
+		count = 0.0
+		for times in range(iteration):
+			resp_new,cnt_new = progress(TaskQueue,ExecutionList)
+			resp += resp_new
+			count += cnt_new
+			genTask(2)
+			while(True):
+				observation = getObservation()
+				action = Pgt.choose_action(observation)
+				actions[action]+=1	
+				#print(action)
+				#print(times,action)
+				result = execution(action) 
+				reward,average = rewardsignal(ExecutionList)
+				
+				if(result == 0):
+						#print(times,action)
+						#cts.append(1.0/average)
+						break
+		#print resp,count
+		counts.append(resp/count)
+
+		#print(counts)
+	return sum(counts)/len(counts),actions	
 def run():
+	iteration = 500
 	rw = []
-	iteration = 30000
+	counts = []
+	for ite in range(iteration):
+		#np.random.seed(7)
+		actions,rewards = train()
+		l1,l2 = Pgt.get_weights()
+		#print(l1)
+		#print("---------")
+		#print(l2)
+
+		#rewards = [rewards[x]/max(actions[x],1) for x in range(len(actions))]
+		ma = zip(actions,rewards)
+		#print(ite,ma)
+		#rw.append(reward)
+		average_count,actions = eval_policy()
+		print(ite,sum(rewards)/len(rewards),average_count,actions)
+		print("\n")
+		counts.append(average_count)
+
+
+	for i in range(300):
+		obs = np.random.random_sample(12)*16
+		action = Pgt.choose_action(obs)
+		print(action)
+	traj = []
+	global resource_pool
+	global TaskQueue
+	global ExecutionList
+	resource_pool[0] = 1.0
+	resource_pool[1] = 1.0
+	TaskQueue = list()
+	ExecutionList = list()
+	# np.random.seed(10)
+	# for times in range(300):
+	# 	progress(TaskQueue,ExecutionList)
+	# 	genTask(2)
+	# 	for task in TaskQueue:
+	# 		if(task.memory_demand <= resource_pool[1]):
+	# 				TaskQueue.remove(task)
+	# 				resource_pool[0] -= task.cpu_demand
+	# 				resource_pool[1] -= task.memory_demand
+	# 				ExecutionList.append(task)
+	# 	string = ""
+	# 	for task in TaskQueue:
+	# 		string+=' '+str(task.memory_demand)
+	# 	print(string)
+	# 	traj.append(len(TaskQueue))
+	#layer1,layer2 = Pgt.get_weights()
+	#print(layer1)
+	#print("-----------")
+	#print(layer2)
+
+
+	# plt.plot(traj)
+	#cost = Pgt.plot_lost()
+	#plt.plot(cost)
+	# plt.show()
+
+
+def run_train():
+	rw = []
+	ct = []
+	iteration = 5000
 	for times in range(iteration):
 		progress(TaskQueue,ExecutionList)
-		genTask(5)
+		genTask(2)
 		#print("here!!!!")
 		avgs = []
-
+		counts = []
 		while(True):
-			observation = getObservation()
+			exec_list,observation = getObservation()
+
 			action = Pgt.choose_action(observation)
+			#for i in range(min(len(exec_list),10)):
+			#	print(exec_list[i].execution_Time)
+			#print(len(exec_list),action,resource_pool[0],resource_pool[1])
 			#print(action,len(TaskQueue),resource_pool[0],resource_pool[1])
-			result = execution(action) 
-			if not result:
-				action = 0
+		#	action = 1
+			result = execution(action,exec_list) 
+
+			#if not result: 
+			#	action = 0
 			reward,average = rewardsignal(ExecutionList)
 			avgs.append(average)
+			#if(average == 0):
+			#	average = 1
+			counts.append(average)
 			#print(reward)
 			#for task in ExecutionList:
 			#	print (task.execution_Time)
-			#print(action)
-			if(action == 0):
+			#print(action,reward)
+			if(len(exec_list) == 0):
+				action = 1
+			Pgt.store_transition(observation,action,result)
+			if(result == 0):
+				#print("break!")
 				break
-			Pgt.store_transition(observation,action,reward)
 
-		if((times+1) % 30 == 0):
+		if((times+1) % 20 == 0):
 			#print(reward)
 			#print("time: "+str(time))
 			#start = time.time()
 			#print(sum(avgs)/len(avgs))
 			rw.append(sum(avgs)/len(avgs))
+			ct.append(sum(counts)/len(counts))
+			print(str((times+1)/20)+' '+str(sum(counts)/len(counts))+' '+str(short_task)+' '+str(long_task))
 			avgs = []
-			print(str((times+1)/30))
+			counts = []
 			vt = Pgt.learn()
 			string=""
 			for task in TaskQueue:
@@ -155,7 +403,10 @@ def run():
 	#print("here")
 	##DQN.plot_lost()
 	#Pgt.plot_lost()
-	plt.plot(rw)
+	#plt.plot(rw)
+	#plt.plot(ct,color='red')
+	cost = Pgt.plot_lost()
+	plt.plot(cost)
 	plt.show()
 
 
